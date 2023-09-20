ea5106e662e6bd3a3f9c889bfc9b2b4c84bef76a
Ivan <145473581+qboshenkiy@users.noreply.github.com>
2023-09-20 19:52:55 +0600

    0.2

--- react-files/src/App.js
+++ react-files/src/App.js
@@ -1,8 +1,10 @@
+import CostItem from "./components/CostItem";
 
 function App() {
   return (
     <div>
       <h1>Изучение React</h1>
+      <CostItem></CostItem>
     </div>
   );
 }
---
+++ react-files/src/components/CostItem.js
@@ -0,0 +1,14 @@
+import './CostItem.css';
+function CostItem() {
+  return (
+    <div className='cost-item'>
+      <p></p>
+      <div className='cost-item__description'>
+        <h2></h2>
+        <div className='cost-item__price'>$</div>
+      </div>
+    </div>
+  );
+}
+
+export default CostItem;
