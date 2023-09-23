eaa9c130dd533743c1dc16426228eebdb6a0d915
Ivan <145473581+qboshenkiy@users.noreply.github.com>
2023-09-23 18:45:28 +0600

    check

--- react-files/src/components/NewCost/CostForm.js
+++ react-files/src/components/NewCost/CostForm.js
@@ -67,38 +67,7 @@ const CostForm = (props) => {
   };
 
   return (
-    <form onSubmit={sumbitHandler}>
-      <div className="new-cost__controls">
-        <div className="new-cost__control">
-          <label>Название</label>
-          <input type="text" value={inputName} onChange={nameChangeHandler} />
-        </div>
-        <div className="new-cost__control">
-          <label>Сумма</label>
-          <input
-            type="number"
-            min="0.01"
-            step="0.01"
-            value={inputAmount}
-            onChange={amountChangeHandler}
-          />
-        </div>
-        <div className="new-cost__control">
-          <label>Дата</label>
-          <input
-            type="date"
-            min="2023-01-01"
-            step="2024-12-31"
-            value={inputDate}
-            onChange={dateChangeHandler}
-          />
-        </div>
-        <div className="new-cost__actions">
-          <button type="submit">Добавить Расход</button>
-          <button type="submit">Отмена</button>
-        </div>
-      </div>
-    </form>
+    
   );
 };
 export default CostForm;
